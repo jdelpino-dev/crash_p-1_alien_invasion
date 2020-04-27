74fdbec45b276274a260c02370f0f0a5e083f277
jdelpino <delpinoivivas@gmail.com>
2020-04-27 14:21:03 -0500

    Piloting the ship v4.

--- alien_invasion.py
+++ alien_invasion.py
@@ -44,9 +44,21 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                if event.key == pygame.K_UP:
+                    self.ship.moving_fwd = True
+                if event.key == pygame.K_DOWN:
+                    self.ship.moving_bck = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                if event.key == pygame.K_UP:
+                    self.ship.moving_fwd = False
+                if event.key == pygame.K_DOWN:
+                    self.ship.moving_bck = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
--- ship.py
+++ ship.py
@@ -25,13 +25,22 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Movement flag.
+        # Movement flags.
         self.moving_right = False
+        self.moving_left = False
+        self.moving_fwd = False
+        self.moving_bck = False
 
     def update(self):
-        """Update the ship's position based on the movement flag."""
+        """Update the ship's position based on the movement flags."""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+        if self.moving_fwd:
+            self.rect.y -= 1
+        if self.moving_bck:
+            self.rect.y += 1
 
     def blitme(self):
         """Draw the ship at its current location."""
