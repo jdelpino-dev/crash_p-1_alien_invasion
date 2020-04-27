d535944a3a13d26d39daa585c8f09fe5e08ca5e3
jdelpino <delpinoivivas@gmail.com>
2020-04-27 11:06:47 -0500

    Creating the Ship Class.

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,9 +22,9 @@ class AlienInvasion:
         def __init__(self):
             """Initialize the game, and create game resources."""
             pygame.init()
-            self.Settings = Settings()
+            self.settings = Settings()
             self.screen = pygame.display.set_mode(
-                (Settings.screen_width, Settings.screen_height))
+                (self.settings.screen_width, self.settings.screen_height))
             pygame.display.set_caption("Alien Invasion")
             self.ship = Ship(self)
             # Set the background color.
@@ -39,7 +39,7 @@ class AlienInvasion:
 
                 # Redraw the screen –with its elements– during each pass
                 # through the loop.
-                self.screen.fill(Settings.bg_color)
+                self.screen.fill(self.settings.bg_color)
                 self.ship.blitme()
 
                 # Make the most recently drawn screen visible.
