fc00e1fb35f2ac579574d00a5b3ae09903243617
jdelpino <delpinoivivas@gmail.com>
2020-04-27 13:20:20 -0500

    Piloting the ship v2

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,6 +33,7 @@ class AlienInvasion:
             """Start the main loop for the game."""
             while True:
                 self._check_events()
+                self.ship.update()
                 self._update_screen()
 
         def _check_events(self):
@@ -42,8 +43,10 @@ class AlienInvasion:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
-                        # Move the ship to the right.
-                        self.ship.rect.x += 1
+                        self.ship.moving_right = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
 
         def _update_screen(self):
             """Update images on the screen, and flip to the new screen."""
