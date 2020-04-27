483d29d2ce3d39ac0bbff02959d437787205a2f6
jdelpino <delpinoivivas@gmail.com>
2020-04-27 09:29:36 -0500

    Backgroun color.

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,9 +19,10 @@ class AlienInvasion:
         def __init__(self):
             """Initialize the game, and create game resources."""
             pygame.init()
-
             self.screen = pygame.display.set_mode((1200, 800))
             pygame.display.set_caption("Alien Invasion")
+            # Set the background color.
+            self.bg_color = (230, 230, 230)
 
         def run_game(self):
             """Start the main loop for the game."""
@@ -30,6 +31,9 @@ class AlienInvasion:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         sys.exit()
+
+                # Redraw the screen during each pass through the loop.
+                self.screen.fill(self.bg_color)
 
                 # Make the most recently drawn screen visible.
                 pygame.display.flip()
