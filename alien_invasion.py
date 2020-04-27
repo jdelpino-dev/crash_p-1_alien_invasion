7bf60af1a88b1ef66bebf1ba4840f1a0811ea0e0
jdelpino <delpinoivivas@gmail.com>
2020-04-27 16:39:09 -0500

    Refactoring _check_events()

--- alien_invasion.py
+++ alien_invasion.py
@@ -42,23 +42,31 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-                if event.key == pygame.K_UP:
-                    self.ship.moving_fwd = True
-                if event.key == pygame.K_DOWN:
-                    self.ship.moving_bck = True
+                _check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
-                if event.key == pygame.K_UP:
-                    self.ship.moving_fwd = False
-                if event.key == pygame.K_DOWN:
-                    self.ship.moving_bck = False
+                _check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        if event.key == pygame.K_UP:
+            self.ship.moving_fwd = True
+        if event.key == pygame.K_DOWN:
+            self.ship.moving_bck = True
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        if event.key == pygame.K_UP:
+            self.ship.moving_fwd = False
+        if event.key == pygame.K_DOWN:
+            self.ship.moving_bck = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
