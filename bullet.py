fa60389ce183522be3f00a3fe766d456b3de9005
jdelpino <delpinoivivas@gmail.com>
2020-04-28 12:23:03 -0500

    Creating the Bullet Class!

---
+++ bullet.py
@@ -0,0 +1,45 @@
+#! /opt/anaconda3/envs/learningp37/bin/python
+# Project # 1 / From "Python Crash Course" (2nd Edition), by Eric Matthes
+# Code by Eric Matthes and José Delpino
+# PROJECT # 1: Alien Invasion
+# Chapter # 12: A Ship that Fires Bullets
+
+# BULLET CLASS
+"""Ship module that contains the class Ship. Adds the ability to shoot
+bullets. Each bullet is represented by a small rectangle, when the player
+presses the spacebar. Bullets travel straight up the screen
+until they disappear off the top of the screen."""
+
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+
+    def __init__(self, ai_game):
+        """Create a bullet object at the ship's current position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # Create a bullet rect at (0, 0) and then set correct position.
+        self.rect = pygame.Rect(0, 0,
+                                self.settings.bullet_width,
+                                self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        # Store the bullet's position as a decimal value.
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet.
+        self.y -= self.settings.bullet_speed
+        # Update the rect position.
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
